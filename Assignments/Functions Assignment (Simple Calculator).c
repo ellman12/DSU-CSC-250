d77185c981169a5b4f4fd4ea436055abe7fb434c
ellman12 <elldc4@gmail.com>
2020-09-02 14:39:43 -0500

    Finished functions assignment

--- Assignments/Functions Assignment (Simple Calculator).c
+++ Assignments/Functions Assignment (Simple Calculator).c
@@ -1,9 +1,9 @@
 ///////////////////////////////////////////////////////////////////////
-//File: Functions Assignment.c
+//File: Functions Assignment (Simple Calculator).c
 ///////////////////////////////////////////////////////////////////////
 //Purpose: File for the Functions Assignment. It is a basic calculator.
 ///////////////////////////////////////////////////////////////////////
-//Programmer: Elliott DuCharme
+//Programmer: Elliott DuCharme for Computer Science II (CSC 250).
 ///////////////////////////////////////////////////////////////////////
 //Due Date: Sep. 9th 2020
 ///////////////////////////////////////////////////////////////////////
@@ -15,59 +15,59 @@
 ///////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 
-float add(float num1, float num2);
-float subtract(float num1, float num2); // a - b.
-float multiply(float num1, float num2);
-float divide(float num1, float num2); // a / b
-
-//alt method:
-
-// do the math and print out after switch statement
+//Function prototyping.
+float add(float num1, float num2);      // return a + b.
+float subtract(float num1, float num2); // return a - b.
+float multiply(float num1, float num2); // return a * b.
+float divide(float num1, float num2);   // return a / b.
 
 int main()
 {
-    float num1, num2;
-    int userChoice, proramRunning = 1;
+    float num1, num2;      //Numbers the user inputs.
+    int userChoice;        //What the user wants to do in the program.
+    int proramRunning = 1; //1 while program is running; 0 when it's not.
 
+    //Runs until the user says to quit.
     while (proramRunning == 1)
     {
-        printf("Please choose one of the following options:\n");
+        printf("Please choose one of the following options:\n\n");
         printf("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Quit the program\n");
         scanf("%d", &userChoice);
 
+        //Program decides what operation to do.
         switch (userChoice)
         {
         case 1:
-            printf("Enter two numbers to add: ");
+            printf("\nEnter two numbers to add: ");
             scanf("%f %f", &num1, &num2);
-            printf("The answer is %f\n", add(num1, num2));
+            printf("\nThe answer is %f\n\n", add(num1, num2));
             break;
 
         case 2:
-            printf("Enter two numbers to subtract: ");
+            printf("\nEnter two numbers to subtract: ");
             scanf("%f %f", &num1, &num2);
-            printf("The answer is %f\n", subtract(num1, num2));
+            printf("\nThe answer is %f\n\n", subtract(num1, num2));
             break;
 
         case 3:
-            printf("Enter two numbers to multiply: ");
+            printf("\nEnter two numbers to multiply: ");
             scanf("%f %f", &num1, &num2);
-            printf("The answer is %f\n", multiply(num1, num2));
+            printf("\nThe answer is %f\n\n", multiply(num1, num2));
             break;
 
         case 4:
-            printf("Enter two numbers to divide: ");
+            printf("\nEnter two numbers to divide: ");
             scanf("%f %f", &num1, &num2);
-            printf("The answer is %f\n", divide(num1, num2));
+            printf("\nThe answer is %f\n\n", divide(num1, num2));
             break;
 
         case 5:
             proramRunning = 0;
-            printf("Thank you for using the program. Goodbye!");
+            printf("Thank you for using the program. Goodbye!\n");
             break;
 
         default:
-            printf("Error. That is not a choice.");
+            printf("Error. That is not a choice.\n\n");
             break;
         }
     }
