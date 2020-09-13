aa8a0ef21e9fb48e289a145d421f172320b2f8ce
ellman12 <elldc4@gmail.com>
2020-09-13 14:35:50 -0500

    homework done

--- Assignments/Recursion Assignment.c
+++ Assignments/Recursion Assignment.c
@@ -7,41 +7,40 @@
 ///////////////////////////////////////////////////////////////////////
 //Due Date: Sep. 16th, 2020
 ///////////////////////////////////////////////////////////////////////
-//Creation date: Sep. 15th, 2020
+//Creation date: Sep. 12th, 2020
 ///////////////////////////////////////////////////////////////////////
-//Comments: https://d2l.sdbor.edu/d2l/lms/dropbox/user/folder_submit_files.d2l?db=871844&grpid=0&isprv=0&bp=0&ou=1453155
+//Comments: https://d2l.sdbor.edu/d2l/lms/dropbox/user/folder_submit_files.d2l?db=873465&grpid=0&isprv=0&bp=0&ou=1453155
 ///////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 
-int power(int base, int exponent);
+float power(float base, float exponent);
 
 int main()
 {
-    int userBase, userExponent; //What the user enters.
-    printf("Enter the base: ");
-    scanf("%d", &userBase);
+    float userBase, userExponent; //What the user enters.
+    float result;                 //Result after calling power function.
 
+    printf("\nEnter the base: ");
+    scanf("%f", &userBase);
     printf("Enter the exponent: ");
-    scanf("%d", &userExponent);
+    scanf("%f", &userExponent);
 
-    int result = power(userBase, userExponent);
-    printf("The result is %d\n", result);
+    result = power(userBase, userExponent);
+    printf("%f^%f = %f.\n\n", userBase, userExponent, result);
 
     return 0;
 }
 
 //Raises the base to a specified exponent. E.g., 3^4 = 81.
-int power(int base, int exponent)
+float power(float base, float exponent)
 {
-    if (exponent == 0 && base != 0)
-    {
-        return 1; //Any nonzero number raised to 0 becomes 1.
-    }
-    else if (exponent == 0 && base == 0)
-    {
-        printf("ERROR. 0^0 is undefined!");
-        return; //Can't do this operation, so don't return a number.
-    }
-    else
+
+    if (exponent > 0)
         return base * power(base, exponent - 1);
+
+    else if (exponent < 0)
+        return 1 / power(base, -exponent);
+
+    else
+        return 1;
 }
