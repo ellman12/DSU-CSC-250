38c836bdc86a8b3e4ca75ca74516103be759bbed
ellman12 <elldc4@gmail.com>
2020-09-13 16:01:58 -0500

    finished assignment

--- Assignments/Recursion Assignment.c
+++ Assignments/Recursion Assignment.c
@@ -26,7 +26,7 @@ int main()
     scanf("%f", &userExponent);
 
     result = power(userBase, userExponent);
-    printf("%f^%f = %f.\n\n", userBase, userExponent, result);
+    printf("\n%f^%f = %f.\n", userBase, userExponent, result);
 
     return 0;
 }
@@ -35,12 +35,21 @@ int main()
 float power(float base, float exponent)
 {
 
-    if (exponent > 0)
+    if (exponent > 0) //E.g., 3^4 = 81.
         return base * power(base, exponent - 1);
 
-    else if (exponent < 0)
+    else if (exponent < 0) //E.g., 3^-2 = 1/(3^2) = 1/9.
         return 1 / power(base, -exponent);
 
-    else
+    else if (base == 0) //E.g., 0^1, etc. x^0 = 1.
+        return 0;
+
+    else if (exponent == 0) //E.g., 1^0. x^0 = 1.
         return 1;
+
+    else if (base == 0 && exponent == 0) //0^0
+    {
+        printf("Technically, this should be undefined. But 0^0 can also be defined as equaling 1.\n");
+        return 1;
+    }
 }
