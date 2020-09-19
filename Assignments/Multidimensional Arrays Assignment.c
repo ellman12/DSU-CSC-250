5a605afa0658c4fac1e7d06171e940d1bbbc8dfa
ellman12 <elldc4@gmail.com>
2020-09-18 20:39:48 -0500

    work on assignmet

--- Assignments/Multidimensional Arrays Assignment.c
+++ Assignments/Multidimensional Arrays Assignment.c
@@ -5,7 +5,7 @@
 ///////////////////////////////////////////////////////////////////////
 //Programmer: Elliott DuCharme for Computer Science II (CSC 250).
 ///////////////////////////////////////////////////////////////////////
-//Due Date: Unknown
+//Due Date: 25th.
 ///////////////////////////////////////////////////////////////////////
 //Created: Wednesday, September 16, 2020.
 ///////////////////////////////////////////////////////////////////////
@@ -15,10 +15,8 @@
 #include <time.h>
 #include <stdlib.h>
 
-int main()
+float randPrice()
 {
-    srand(time(NULL));
-
     int MIN = 0, MAX = 20000; //dollars
     float whole = rand() % (MAX + 1 - MIN) + MIN;
     float number, num1, num2;
@@ -28,8 +26,31 @@ int main()
         number = num1 / num2;
     else
         number = num2 / num1;
+
     whole = whole + number;
-    printf("\n$%.2f\n", whole);
+    return whole;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    //One name in each row.
+    char customers[5][10] = {"Willie", "Kelly", "Terry", "Jordan", "Taylor"};
+
+    //Twelve months (rows), five columns with dollar values for the 5 customers.
+    float monthlyBalances[12][5];
+
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            monthlyBalances[i][j] = randPrice();
+            printf("customers[%d][%d] = $ .2%f\n", i, j, monthlyBalances[i][j]);
+        }
+
+        printf("\n");
+    }
 
     return 0;
 }
