6c369f2b944500ec3b26a10e802a3770688034ad
ellman12 <elldc4@gmail.com>
2020-09-20 20:08:00 -0500

    partially working

--- Assignments/Multidimensional Arrays Assignment.c
+++ Assignments/Multidimensional Arrays Assignment.c
@@ -35,21 +35,42 @@ int main()
 {
     srand(time(NULL));
 
+    int i, j;
+
     //One name in each row.
     char customers[5][10] = {"Willie", "Kelly", "Terry", "Jordan", "Taylor"};
+
+    float averages[5];
 
     //Twelve months (rows), five columns with dollar values for the 5 customers.
     float monthlyBalances[12][5];
 
-    for (int i = 0; i < 12; i++)
+    for (i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (j = 0; j < 10; j++)
+        {
+            printf("%c", customers[i][j]);
+        }
+
+        printf("\t\t\t");
+    }
+
+    printf("\n");
+
+    for (i = 0; i < 12; i++)
+    {
+        for (j = 0; j < 5; j++)
         {
             monthlyBalances[i][j] = randPrice();
-            printf("customers[%d][%d] = $ %.2f\n", i, j, monthlyBalances[i][j]);
+            averages[i] += monthlyBalances[i][j];
+            printf("Month %d balance: $ %.2f\t", i + 1, monthlyBalances[i][j]);
         }
 
         printf("\n");
+    }
+
+    for (i = 0; i < 5; i++)
+    {
     }
 
     return 0;
