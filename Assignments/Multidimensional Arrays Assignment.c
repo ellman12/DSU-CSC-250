322fbb90b40b730cf58fcccc80c23acf06987236
ellman12 <ellduc4@gmail.com>
2020-09-22 15:24:11 -0500

    good progress on the 2 other extra credit

--- Assignments/Multidimensional Arrays Assignment.c
+++ Assignments/Multidimensional Arrays Assignment.c
@@ -16,6 +16,18 @@
 #include <time.h>
 #include <stdlib.h>
 
+int programRunning = 1;
+float monthlySummary; //For each month, this monthly summary is printed next to the five people in its own column.
+
+//One name in each row, plus the summary column.
+char columns[6][15] = {"Willie", "Kelly", "Terry", "Jordan", "Taylor", "Monthly Summary"};
+
+//Stores averages for the five customers for the whole year.
+float annualAverages[5];
+
+//Twelve months (rows), five columns with dollar values for the 5 customers.
+float monthlyBalances[12][5];
+
 float randPrice() //Return a pseudorandom price.
 {
     int MIN = 0, MAX = 20000; //dollars
@@ -32,26 +44,12 @@ float randPrice() //Return a pseudorandom price.
     return whole;
 }
 
-int main()
+void printArrays()
 {
-    srand(time(NULL));
-
-    int i, j;
-    float monthlySummary; //For each month, this monthly summary is printed next to the five people in its own column.
-
-    //One name in each row, plus the summary column.
-    char columns[6][15] = {"Willie", "Kelly", "Terry", "Jordan", "Taylor", "Monthly Summary"};
-
-    //Stores averages for the five customers for the whole year.
-    float annualAverages[5];
-
-    //Twelve months (rows), five columns with dollar values for the 5 customers.
-    float monthlyBalances[12][5];
-
     printf("\n"); //Print out all six columns (five people and the monthly summary).
-    for (i = 0; i < 6; i++)
+    for (int i = 0; i < 6; i++)
     {
-        for (j = 0; j < 11; j++)
+        for (int j = 0; j < 11; j++)
         {
             printf("%c", columns[i][j]);
         }
@@ -60,9 +58,9 @@ int main()
     }
 
     printf("\n"); //Print out the balances and the summary.
-    for (i = 0; i < 12; i++)
+    for (int i = 0; i < 12; i++)
     {
-        for (j = 0; j < 5; j++)
+        for (int j = 0; j < 5; j++)
         {
             monthlyBalances[i][j] = randPrice();
             monthlySummary += monthlyBalances[i][j];
@@ -84,12 +82,67 @@ int main()
 
     printf("\nAverage monthly balances:\n");
 
-    for (i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
     {
         printf("\t  $ %.2f\t", annualAverages[i]);
     }
 
     printf("\n");
+}
+
+// void editDollarValue(int month); //TODO
+
+int main()
+{
+    srand(time(NULL));
+
+    int menuChoice;       //User picking what they want to do in the program.
+    int customerChoice;   //Number of the user they want to edit.
+    int monthChoice;      //User chooses what month they want to edit.
+    float newDollarValue; //User enters new dollar value they want at that month for that user.
+
+    while (programRunning == 1)
+    {
+        printf("\n1. Print the values in the arrays.\n2. Edit a dollar value.\n3. Quit.\n\nWhat would you like to do? ");
+        scanf("%d", &menuChoice);
+
+        if (menuChoice == 1)
+        {
+            printArrays();
+        }
+
+        else if (menuChoice == 2) //Edit value.
+        {
+            printf("Choose a customer (enter a number): \n");
+            scanf("%d", &customerChoice);
+
+            if (customerChoice > 5 || customerChoice < 1)
+            {
+                printf("\nError. Invalid choice.");
+                // continue;
+            }
+
+            printf("\nEnter a month number:\n\n1. January\n2. February\n3. March\n4. April\n5. May\n6. June\n7. July\n8. August\n9. September\n10. October\n11. November\n12. December\n\n");
+            scanf("%d", &menuChoice);
+
+            if (menuChoice > 12 || menuChoice < 1)
+            {
+                printf("\nError. Invalid choice.");
+                // continue;
+            }
+        }
+
+        else if (menuChoice == 3) //Quit.
+        {
+            printf("Goodbye!\n");
+            programRunning = 0;
+        }
+
+        else
+        {
+            printf("\nError. Invalid choice.");
+        }
+    }
 
     return 0;
 }
