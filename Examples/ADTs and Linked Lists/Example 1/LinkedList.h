7024ddac5777964ee7554c65dbf0017ea604e2c6
Elliott DuCharme <ellduc4@gmail.com>
2020-11-02 11:50:42 -0600

    in class stuff

--- Examples/ADTs and Linked Lists/Example 1/LinkedList.h
+++ Examples/ADTs and Linked Lists/Example 1/LinkedList.h
@@ -10,14 +10,14 @@ typedef struct
     struct node *next;
 } node;
 
-// head is the beginning of our linked list.
-// d is the data to put in our newNode.
-// index is where we want to insert the new node.
-// size is how big our linked list is.
+//head is the beginning of our linked list.
+//d is the data to put in our newNode.
+//index is where we want to insert the new node.
+//size is how big our linked list is.
 node *insertNode(node *head, int d, int index, int size)
 {
-    // If there is one node in the list, then size = 1.
-    // If size is zero, and index is zero, that's OK.
+    //If there is one node in the list, then size = 1.
+    //If size is zero, and index is zero, that's OK.
     if (index > size)
     {
         printf("%d is too big, our list size is %d.\n", index, size);
@@ -28,13 +28,36 @@ node *insertNode(node *head, int d, int index, int size)
     newNode->data = d;
     newNode->next = NULL;
 
-    // Check if we're inserting at the head node.
+    //Check if we're inserting at the head node.
     if (index == 0)
     {
         newNode->next = head;
         head = newNode;
         return head;
     }
+
+    //Inserting at index, need to stop short by one.
+    node *temp = head;
+    for (int i = 0; i < index - 1; i++)
+    {
+        temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    return head;
+}
+
+void printList(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d  ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
 }
 
 #endif
--- Examples/ADTs and Linked Lists/Example 1/ListMain.c
+++ Examples/ADTs and Linked Lists/Example 1/ListMain.c
@@ -5,10 +5,26 @@
 ///////////////////////////////////////////////////////////////////////
 //Created: Wednesday, October 28, 2020 11:05 AM.
 ///////////////////////////////////////////////////////////////////////
-#include <stdio.h>
+#include "LinkedList.h"
 
 int main()
 {
-    
+    int n, d, index, size = 0;
+    node *head = NULL;
+    printf("How many values would you like to add? ");
+    scanf("%d", &n);
+    printf("\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter a value: ");
+        scanf("%d", &d);
+        printf("\nWhat index would you like to insert at? ");
+        scanf("%d", &index);
+
+        head = insertNode(head, d, index, size);
+        size++;
+        printList(head);
+    }
     return 0;
 }
